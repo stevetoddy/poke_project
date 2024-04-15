5c5fc7c4be71150f45a4a8573afb5ecec7b5a868
Steve <stevetoddy@gmail.com>
2024-04-15 14:34:36 +1000

    Add layout with name, sound and placeholder. Add states to hold reroll count

--- poke-project/src/App.tsx
+++ poke-project/src/App.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import pokeHeading from '../public/International_Pokémon_logo.svg.png'
+import pokeBall from '../public/pokeball.svg'
 import ReactAudioPlayer from 'react-audio-player';
 
 
@@ -11,10 +12,12 @@ function App() {
   const [pokemonOne, setPokemonOne] = useState<string>()
   const [pokemonTwo, setPokemonTwo] = useState<string>()
   const [typesOne, setTypesOne] = useState([])
-  const [typesTwo, setTypesTwo] : any = useState([])
+  const [typesTwo, setTypesTwo] = useState([])
   const [nameOne, setNameOne] = useState<string>()
+  const [nameTwo, setNameTwo] = useState<string>()
   const [cry, setCry] = useState<string>()
-
+  const [rerollsOne, setRerollsOne] = useState<number>(0)
+  const [rerollsTwo, setRerollsTwo] = useState<number>(0)
 
   useEffect(() => {
     axios.get(baseURL).then((response) => {
@@ -34,86 +37,118 @@ function App() {
 
   const handleClickOne = () => {
     const randomNumberOne = Math.floor(Math.random() * 160)
-
-    axios.get(pokeURLs[randomNumberOne]).then((response) => {
-      setNameOne(response.data.name)
-      setCry(response.data.cries.latest)
-      setPokemonOne(response.data.sprites.front_default)
-    });
+    if (rerollsOne < 3) {
+      axios.get(pokeURLs[randomNumberOne]).then((response) => {
+        setTypesOne([])
+        setTypesOne(response.data.types)
+        setNameOne(response.data.name)
+        setCry(response.data.cries.latest)
+        setPokemonOne(response.data.sprites.front_default)
+        setRerollsOne(rerollsOne + 1)
+      });
+    } else {
+      alert("You have no rerolls left :'(")
+    }
   }
 
   const handleClickTwo = () => {
     const randomNumberTwo = Math.floor(Math.random() * 160)
 
-    axios.get(pokeURLs[randomNumberTwo]).then((response) => {
-      setTypesTwo([])
-      setTypesTwo(response.data.types)
-      setCry(response.data.cries.latest)
-      // console.log(typesTwo)
-      setPokemonTwo(response.data.sprites.front_default)
-    });
-
-
+    if (rerollsTwo < 3) {
+      axios.get(pokeURLs[randomNumberTwo]).then((response) => {
+        setTypesTwo([])
+        setTypesTwo(response.data.types)
+        setNameTwo(response.data.name)
+        setCry(response.data.cries.latest)
+        setPokemonTwo(response.data.sprites.front_default)
+        setRerollsTwo(rerollsTwo + 1)
+      });
+    } else {
+      alert("You have no rerolls left :'(")
+    }
   }
 
   const getTypes = () => {
     // console.log(typesTwo)
-    typesTwo.forEach((type : any ) => {
-      console.log(type.type.name)
+    typesOne.forEach((type : any ) => {
+      console.log('One: ', type.type.name)
     })
+    typesTwo.forEach((type : any ) => {
+      console.log('Two: ', type.type.name)
+    })
+
+    setRerollsOne(0)
+    setRerollsTwo(0)
   }
 
   return (
     <>
-    <div className="flex flex-col w-full items-center">
-      <img className="mt-10 max-w-[600px]" src={pokeHeading} alt="" />
-      <h1 className="text-2xl font-black text-[#0075BE]">BATTLER</h1>
-    </div>
-    <div className="flex">
-      <div className="flex flex-col items-center h-full w-full">
-        {!pokemonOne ? 
-          <div className="flex align-center items-center h-32">
-            <h1 className="text-2xl font-black text-[#0075BE]">Choose Your Pokemon</h1>
-          </div> : 
-          <>
-            <img className="w-32 h-32 mt-5" src={pokemonOne} alt="" />
-            <h1 className="text-2xl font-black text-[#0075BE] capitalize">{nameOne}</h1>
-          </>
-          
+      <div className="flex flex-col w-full items-center">
+        <img className="mt-10 max-w-[600px]" src={pokeHeading} alt="" />
+        <h1 className="text-[42px] m-[-20px] font-black text-[#0075BE]">BATTLER</h1>
+      </div>
+      <div className="flex mt-20">
+
+
+        <div className="flex flex-col items-center h-full w-full">
+          {!pokemonOne ? 
+            <div className="flex flex-col items-center w-[260px h-[260px]">
+              <img src={pokeBall} alt="pokeball" className='w-52' /> 
+              <h1 className="text-2xl font-black text-[#0075BE] mt-5">Choose Your Pokemon</h1>
+            </div> : 
+            <div className='flex flex-col w-[260px h-[260px]'>
+              <img className="w-52 h-52 mt-5" src={pokemonOne} alt="" />
+              <h1 className="text-2xl text-center font-black text-[#0075BE] capitalize">{nameOne}</h1>
+            </div>
+            }
+          <button 
+            className='flex justify-center w-96 h-12 bg-[#0075BE] p-3 rounded mt-10' 
+            onClick={handleClickOne}>
+              CLICK ME
+          </button>
+          {rerollsOne === 0 ? 
+            '' : 
+            <div className="text-2xl font-black text-[#0075BE] mt-5">Rerolls left: {3 - rerollsOne}</div>  
           }
-        <button 
-          className='flex justify-center w-96 h-12 bg-[#0075BE] p-3 rounded mt-10' 
-          onClick={handleClickOne}>
-            CLICK ME
-        </button>
-      </div>
-      <div className="flex flex-col items-center w-full">
-        {!pokemonTwo ? 
-          '' : 
-          <>
-            <img className="w-32 h-32 mt-5" src={pokemonTwo} alt="" />
-
-          </>}
-        <button 
-          className='flex justify-center w-96 h-12 bg-[#0075BE] p-3 rounded mt-10' 
-          onClick={handleClickTwo}>
-            CLICK ME
-        </button>
-        <button 
-          className='flex justify-center w-96 h-12 bg-[#0075BE] p-3 rounded mt-10' 
-          onClick={getTypes}>
-            TYPES
-        </button>
-        <div className="hidden">
-          <ReactAudioPlayer
-            src={cry}
-            autoPlay = {true}
-            controls
-          />
         </div>
-
+       
+       
+        <div className="flex flex-col items-center h-full w-full">
+          {!pokemonTwo ? 
+            <div className="flex flex-col items-center w-[260px h-[260px]">
+              <img src={pokeBall} alt="pokeball" className='w-52' /> 
+              <h1 className="text-2xl font-black text-[#0075BE] mt-5">Choose Your Pokemon</h1>
+            </div> : 
+            <div className='flex flex-col w-[260px h-[260px]'>
+              <img className="w-52 h-52 mt-5" src={pokemonTwo} alt="" />
+              <h1 className="text-2xl text-center font-black text-[#0075BE] capitalize">{nameTwo}</h1>
+            </div>
+          }
+          <button 
+            className='flex justify-center w-96 h-12 bg-[#0075BE] p-3 rounded mt-10' 
+            onClick={handleClickTwo}>
+              CLICK ME
+          </button>
+          {rerollsTwo === 0 ? 
+            '' :  
+            <div className="text-2xl font-black text-[#0075BE] mt-5">Rerolls left: {3 - rerollsTwo}</div> 
+          }
+          <div className="hidden">
+            <ReactAudioPlayer
+              src={cry}
+              autoPlay = {true}
+              controls
+            />
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="flex justify-center">
+        <button 
+          className='flex justify-center w-96 h-12 bg-[#0075BE] p-3 rounded mt-20' 
+          onClick={getTypes}>
+            BATTLE
+        </button>
+      </div>
 
     </>
   )
